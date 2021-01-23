997ddf06333b35124ebea94a99d38ee8e6fd8b65
sayale <sayale@cmfitzroy.com.ar>
2021-01-22 21:08:38 -0300

    bootstrap.sh cleanning

--- bootstrap.sh
+++ bootstrap.sh
@@ -35,11 +35,3 @@ if [ ! -x "$(command -v docker)" ]; then
 	# docker service
 	sudo systemctl enable docker
 fi
-
-# sudo docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Clave123@' -e 'MSSQL_PID=Express' -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest-ubuntu 
-
-
-
-
-
-
